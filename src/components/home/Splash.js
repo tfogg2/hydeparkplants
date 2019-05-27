6f675badd4b3fad467da62c231dfcfd0cf751e4a
Thomas Fogg <tfogg2@gmail.com>
2019-05-27 15:18:41 -0500

    more text

--- src/components/home/Splash.js
+++ src/components/home/Splash.js
@@ -6,12 +6,12 @@ class Splash extends Component{
     return(
       <div className="home-splash">
         <div className="splash">
-          <img className="splash-logo" src={require('../../assets/hyde-park-logo.png')} />
+          <img className="splash-logo" src={require('../../assets/hpp.com.png')} />
           <div className="splash-copy">
             <p><b>Hyde Park Plants</b>
-               <br/>100% biodegradeable.
-               <br/>Mother of Millions succulent.
-               <br/>Stay tuned & DM if interested.
+               <br/>Now offering the Mother of Millions succulent from Madagascar in a 100% biodegradeable planter.
+               <br/><br/>One for $10 and two for $15. We're able to deliver anywhere in Austin, TX.
+               <br/><br/>We accept Cash, Venmo or Apple Pay. DM if interested.
             </p>
           </div>
         </div>
